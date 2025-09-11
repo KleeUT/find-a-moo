8b819d69ef4427d2f0241bf543baef8a159ac46b
Klee Thomas <klee.ut@gmail.com>
2025-09-11 11:54:32 +1000

    :alembic: adding random to pain text to prove it's caching

--- src/app/plain-text/page.tsx
+++ src/app/plain-text/page.tsx
@@ -3,8 +3,11 @@ import { generateMoos } from "../moo-generator";
 export default function PlainTextPage() {
   const moos = generateMoos(15);
   return (
-    <pre>
-      {moos.map((row) => row.map((cell) => cell.letter).join(" ")).join("\n")}
-    </pre>
+    <>
+      <pre>
+        {moos.map((row) => row.map((cell) => cell.letter).join(" ")).join("\n")}
+      </pre>
+      {Math.random()}
+    </>
   );
 }
